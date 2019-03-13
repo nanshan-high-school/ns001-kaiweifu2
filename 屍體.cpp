a4b67fd1b591bfab6b70014c88763e0d815c10b7
KaKakaKakkkKAkkakkAkkkAkAAkakkakakakakAkAkAkAkkA <45233906+kaiweifu2@users.noreply.github.com>
2019-03-14 00:05:49 +0800

    Create 屍體.cpp

---
+++ 屍體.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+int main() {
+  int totalbook, totalmove, move, book;
+  
+  cout<<"請輸入總共要放的書:";
+  cin>>totalbook;
+  cout<<"請輸入動作次數:";
+  cin>>totalmove;
+  for(int i = 0 ; i <totalmove ; i++){
+    cin>>book;
+    cin>>move;
+    if(move == 'a'){
+       
+    }
+    else if(move == 'b'){
+      
+    }
+    
+  }
+}
